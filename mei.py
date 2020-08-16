e1c488c958235e60a664bf86408f87f325a9b41a
hongmei <duhongmei@protonmail.com>
2020-08-16 23:14:47 +0800

    🚀 created basic command line interface

---
+++ mei.py
@@ -0,0 +1,18 @@
+def mei():
+    '''
+    Basic command line interphase.
+    '''
+
+    text = ''
+
+    while True:   
+        text = input('🌈 mei > ')
+        if text == 'q':
+            print('Existed 🌈 mei')
+            break
+        else:
+            print(text)
+
+
+if __name__ == '__main__':
+    mei()
